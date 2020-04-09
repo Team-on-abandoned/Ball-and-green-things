5ac8b46e53580f184bfa032dc193b8d432316bdb
Team-on <timonsol13@gmail.com>
2020-04-09 20:51:18 +0300

    Init with default project

---
+++ Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using yaSingleton;
+
+[CreateAssetMenu(fileName = "Game Manager", menuName = "Singletons/GameManager")]
+public class GameManager : Singleton<GameManager> {
+	//Properties
+	public Camera Camera { get {
+			if(mainCamera == null) 
+				mainCamera = Camera.main;
+			return mainCamera;
+	}}
+
+	//Other singletons
+	public EventManager Events { get; private set; }
+	public AudioManager audioManager;
+
+	//Global data
+	public string buildNameString;
+
+	[NonSerialized] public Camera mainCamera;
+
+	protected override void Initialize() {
+		base.Initialize();
+
+		mainCamera = Camera.main;
+		Events = new EventManager();
+		Input.multiTouchEnabled = false;
+		LeanTween.init(800);
+
+		EventManager.OnSceneLoadEnd += OnSceneLoadEnd;
+	}
+
+	protected override void Deinitialize() {
+		base.Deinitialize();
+
+		EventManager.OnSceneLoadEnd -= OnSceneLoadEnd;
+	}
+
+	void OnSceneLoadEnd(EventData data) {
+		mainCamera = Camera.main;
+	}
+}
---
+++ Game/Assets/Scripts/UI/Menu/SettingsMenu.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : PopupMenuBase {
+	[Header("Audio settings")]
+	[SerializeField] ToggleGroup enableToggles;
+	[SerializeField] Toggle enableOnToggle;
+	[SerializeField] Toggle enableOffToggle;
+	[SerializeField] Slider mainVolumeSlider;
+	[SerializeField] Slider musicVolumeSlider;
+	[SerializeField] Slider soundVolumeSlider;
+
+	private new void Awake() {
+		base.Awake();
+
+		enableOffToggle.isOn = !AudioManager.Instance.IsEnabled;
+		enableOnToggle.isOn = AudioManager.Instance.IsEnabled;
+		mainVolumeSlider.value = AudioManager.Instance.GetVolume(AudioManager.AudioChannel.Master);
+		musicVolumeSlider.value = AudioManager.Instance.GetVolume(AudioManager.AudioChannel.Music);
+		soundVolumeSlider.value = AudioManager.Instance.GetVolume(AudioManager.AudioChannel.Sound);
+
+		enableOnToggle.onValueChanged.AddListener(OnToggleOnAudio);
+		mainVolumeSlider.onValueChanged.AddListener(OnMainVolumeSliderChange);
+		musicVolumeSlider.onValueChanged.AddListener(OnMusicVolumeSliderChange);
+		soundVolumeSlider.onValueChanged.AddListener(OnSoundVolumeSliderChange);
+	}
+
+	#region Audio Settings
+	public void OnMainVolumeSliderChange(float value) {
+		AudioManager.Instance.SetVolume(AudioManager.AudioChannel.Master, value);
+	}
+
+	public void OnMusicVolumeSliderChange(float value) {
+		AudioManager.Instance.SetVolume(AudioManager.AudioChannel.Music, value);
+	}
+
+	public void OnSoundVolumeSliderChange(float value) {
+		AudioManager.Instance.SetVolume(AudioManager.AudioChannel.Sound, value);
+	}
+
+	public void OnToggleOnAudio(bool value) {
+		AudioManager.Instance.IsEnabled = value;
+	}
+	#endregion
+
+	#region Video
+
+	#endregion
+
+	#region Key bindings
+
+	#endregion
+
+	#region Gameplay
+
+	#endregion
+
+	//TODO: Am I need this? Maybe move it to `general tab` with some other settings
+	#region Language	
+
+	#endregion
+
+	#region Mods
+
+	#endregion
+
+}
