49c72920a9eede5940de6731ebf5cd928ac8e0a0
Team-on <timonsol13@gmail.com>
2020-04-12 01:09:34 +0300

    Enemy animations

--- Game/Assets/Scripts/Enemy.cs
+++ Game/Assets/Scripts/Enemy.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+
+	[Header("Random dance anim")]
+	[SerializeField] byte danceAnimationsCount = 7;
+	Animator anim;
+
+	[Header("Random mesh")]
+	[SerializeField] GameObject capsule;
 	[SerializeField] GameObject[] meshPrefab;
 
-	void Start() {
+	private void Awake() {
+		Destroy(capsule.gameObject);
 
+		byte id = (byte)Random.Range(0, meshPrefab.Length);
+		GameObject spawnedMesh = Instantiate(meshPrefab[id], new Vector3(transform.position.x, 0, transform.position.z), transform.rotation, transform);
+
+		anim = spawnedMesh.GetComponent<Animator>();
+	}
+
+	void Start() {
+		anim.SetInteger("RandomDanceAnim", Random.Range(0, danceAnimationsCount));
 	}
 
 	void Update() {
